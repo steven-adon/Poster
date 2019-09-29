392a33f3fb3ebed3805c9f2a21da4155f970b4cb
John Doe <john@example.com>
2019-09-29 13:32:36 +0800

    upload image

--- src/components/PosterLogo.vue
+++ src/components/PosterLogo.vue
@@ -39,7 +39,7 @@ export default {
     jQuery(function() {
       jQuery('#output').qrcode({
         render: 'canvas', //设置渲染方式，有table和canvas，使用canvas方式渲染性能相对来说比较好
-        text: `https://dwz.cn/8lUbgAcW?shareCode=${shareCode}`, //扫描二维码后显示的内容,可以直接填一个网址，扫描二维码后自动跳向该链接
+        text: `https://dwz.cn/8lUbgAcW?code=${shareCode}`, //扫描二维码后显示的内容,可以直接填一个网址，扫描二维码后自动跳向该链接
         width: 150, //二维码的宽度
         height: 150,
         background: '#fbf7e1', //二维码的后景色
